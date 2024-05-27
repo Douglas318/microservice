eb74d5d74dd3b49eddfca0bfa3c1f2c981adf8e2
Douglas318 <freitas28douglas@gmail.com>
2024-05-26 22:39:34 -0300

    feat: add oders model

--- order-api/core/models.py
+++ order-api/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Oders(models.Model):
+    id = models.AutoField(primary_key=True)
+    users_id = models.IntegerField(null=False)
+    item_description = models.CharField(max_length=150)
+    item_quantity = models.IntegerField()
+    item_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_value = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        managed = False
+        db_table = 'orders'
